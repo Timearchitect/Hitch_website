f0ca1f7beec6f18a0ff2f7b577c9308898447fa3
Abdullah Almasri <abdullahlmsr@gmail.com>
2023-08-25 12:20:08 +0200

    hamburger menu on all html pages
    added hamburger menu to the remaining html pages. created hamburger.js file to avoid conflicts.

--- script.js
+++ script.js
@@ -64,17 +64,4 @@ async function writeToDb(name, surname, email) {
 // */
 
 
-const hamburger = document.querySelector(".hamburger");
-const navMenu = document.querySelector(".nav-menu");
-
-hamburger.addEventListener("click", () => {
-    hamburger.classList.toggle("active");
-    navMenu.classList.toggle("active");
-})
-
-document.querySelectorAll(".nav-link").forEach(n => n.addEventListener("click", () => {
-    hamburger.classList.remove("active");
-    navMenu.classList.remove("active");
-}))
-
 
