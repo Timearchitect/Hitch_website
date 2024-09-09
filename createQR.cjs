d8d6ad7a1f41ab55391b745bab691d10c4dde91a
Jim Lewis Skoglund <124883061+Jimleskog@users.noreply.github.com>
2024-09-09 21:57:08 +0200

    changes

---
+++ createQR.cjs
@@ -0,0 +1,10 @@
+const QRCode = require('qrcode');
+
+// Data to be encoded
+const data = "https://www.hitchapp.se:40888/applink";
+
+// Generating the QR Code
+QRCode.toFile('hitch_applink.png', data, function (err) {
+    if (err) throw err;
+    console.log('QR Code saved to hitchapp_qrcode.png');
+});
